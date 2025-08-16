45e69ee2b8cba51a265b50efaf6ef774b5eb092b
MaxiHer <maxihernandez444@gmail.com>
2025-08-16 14:22:38 -0300

    Separa alert boton

--- eventos.js
+++ eventos.js
@@ -2,4 +2,9 @@ const div = document.querySelector('div')
 
 div.addEventListener("click", function(){
    alert("Hola! Soy el div")
-})
+});
+
+function saludar(e){
+   e.stopPropagation();
+   alert("Hola!");
+}
